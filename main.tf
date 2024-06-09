e462d4828f3fce670f853037ecb008c04bfc2ebe
My Name <myemail@example.com>
2024-06-09 22:41:17 +0530

    d

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
 # data resource to gather information pre-existing resource 
-data "tfe_outputs" "erg"{
+/*data "tfe_outputs" "erg"{
  organization = "SabreADI"
  workspace = "Infraonazure"
+}
+*/
+data "terraform_remote_state" "demo-ResourceGroup"{
+      backend = "remote"
+      config = {
+      organization = "SabreADI"
+      workspace = {
+      name = "Infraonazure"
+                  }
+   }
 }
 
 # Resource Group
@@ -15,9 +25,9 @@ resource "azurerm_virtual_network" "my_terraform_network" {
   name                = "${random_pet.prefix.id}-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
-  #location            = data.tfe_outputs.erg.values.myTFResourceGroupcc1.location
+  #location            = data.terraform_remote_state.demo-ResourceGroup.output.myTFResourceGroupcc1.location
   resource_group_name = azurerm_resource_group.rg.name
-  #resource_group_name = data.tfe_outputs.erg.values.myTFResourceGroupcc1.name
+  #resource_group_name = ata.terraform_remote_state.demo-ResourceGroup.output.myTFResourceGroupcc1.name
   output "rg_name" {
   description = "The name of the created subnet 2."
   value       = data.tfe_outputs.erg.values
